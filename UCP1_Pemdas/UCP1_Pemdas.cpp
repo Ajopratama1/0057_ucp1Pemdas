c119b1631eb0dee6f0bcd9f78f3035aa797b8116
Ajopratama1 <a.ajopratama01@gmail.com>
2024-05-05 11:15:39 +0700

    Add project files.

---
+++ UCP1_Pemdas/UCP1_Pemdas.cpp
@@ -0,0 +1,124 @@
+//1.int nHarga, string Nama, char pilihan
+//2.#include <iostream>
+//using namespace srd;
+// int main () {
+// int a = 20;
+// int b = 10;
+// if  ( a < b ) {
+// cout << "nilai A > B" << endl;
+// } else
+// cout << "nilai B > A " << endl;
+// 
+//3. #include <iostream>
+//using namespace srd;
+// struct buku {
+// string judul;
+// string pengarang;
+// int tahun terbit;
+// }
+// 
+//4.prosedur
+//  #include <iostream>
+//using namespace srd;
+// void cetak pesan() {
+// cout << " pesan dari prosedur " << endl;
+// }
+// int main() {
+// cetak pesan();
+// return 0;
+// }
+// fungsi
+// #include <iostream>
+//using namespace std;
+
+// Deklarasi fungsi
+//int luasPersegiPanjang(int panjang, int lebar) {
+  //  int luas = panjang * lebar;
+    //return luas;
+//}
+
+//int main() {
+  //  int p = 5;
+    //int l = 3;
+
+    // Memanggil fungsi dan menyimpan hasilnya
+    //int hasilLuas = luasPersegiPanjang(p, l);
+
+    //cout << "Luas persegi panjang: " << hasilLuas << endl;
+
+    //return 0;
+//}
+//5.perulangan for
+// #include <iostream>
+//using namespace std;
+
+//int main() {
+
+   // int bilangan;
+
+   // srand(time(0));
+
+    //bilangan = rand() % 12;
+
+   // while (bilangan % 2) {
+     //   cout << bilangan << endl;
+       // cout << "Maka bilangannya ganjil" << endl;
+
+       // bilangan = rand() % 12;
+    //}
+    //cout << bilangan << endl;
+    //cout << "Bilangannya genap" << endl;
+//}
+// perulangan while
+// #include <iostream>
+//using namespace srd;
+
+//int main()
+//{
+  //  int i;
+    //int arr[5];
+
+    //for (i = 0; i < 5; i++)
+    //{
+      //  cout << i << ":" << jodi" << endl;
+    //}
+
+    //for (i = 0; i < 5; i++) {
+      //  cout << "Masukan Nama = ";
+        //cin >> arr[i];
+
+    //}
+    //for (i = 0; i < 5; i++) {
+      //  cout << "Nama ke - " << i << " :  " << arr[i]
+    //}
+//}
+// perulangan do while
+// #include <iostream>
+//using namespace std;
+
+//int main() {
+  //  char pilihan;
+
+    //do {
+      //  cout << "Berangkat ambil takjil" << endl;
+        //cout << "Antri Takjil" << endl;
+        //cout << "Ambil Takjil" << endl;
+        //cout << "Apakah mau antri kembali ?" << endl;
+        //cin >> pilihan;
+   // } while (pilihan == 'y' || pilihan == 'Y');
+//
+//6.
+#include <iostream>
+using namespace std;
+
+Struct mahasiswa{
+    string nama;
+char nMTK;
+char nBI;
+
+}
+
+
+
+
+
